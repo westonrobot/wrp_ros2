f37a93608e4cc471e3f8d7c7ece93d796adb21fb
hanskw4267 <hans.kurnia@westonrobot.com>
2022-07-08 11:46:26 +0800

    peripheral: update headers to use interface instead

--- src/peripheral/imu_sensor_node.cpp
+++ src/peripheral/imu_sensor_node.cpp
@@ -8,6 +8,7 @@
  * Copyright (c) 2021 Weston Robot Pte. Ltd.
  */
 #include "wrp_ros2/peripheral/imu_sensor_node.hpp"
+#include "wrp_sdk/peripheral/imu_sensor_wit.hpp"
 
 namespace westonrobot {
 ImuSensorNode::ImuSensorNode(const rclcpp::NodeOptions& options)
